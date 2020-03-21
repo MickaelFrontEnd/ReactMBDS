a0ad91b665456e40082be0754c85c96d11c8498b
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2020-03-22 00:43:07 +0400

    Add react router

--- MusicBand/music-band/src/App.js
+++ MusicBand/music-band/src/App.js
@@ -1,88 +1,19 @@
-import React, { useState } from 'react';
-import searchByFullText from './services/SearchService';
-import Search from './components/SearchComponent';
-import Tile from './components/TileComponent';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
+import React from 'react';
 import TopBar from './layout/TopBarComponent';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { WASABI_ARTIST_SEARCH } from './cofig/Url';
+import Home from './pages/HomeComponent';
+import Container from '@material-ui/core/Container';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Detail from './pages/DetailComponent';
 
 function App() {
-
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const searchHandler = (text) => {
-    setIsLoading(true);
-    searchByFullText(text)
-      .then((result) => {
-        setData(result);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        alert(error);
-      })
-  };
-
-  const renderLoader = () => {
-    return isLoading && (
-      <Box mx="auto" textAlign="center" pt={5} pb={5}>
-        <CircularProgress />
-      </Box>
-    )
-  }
-
-  const renderNoResult = () => {
-    if (data === undefined || data.length <= 0) {
-      return (
-        <Typography component="div">
-          <Box textAlign="center" pt={5} pb={5} fontSize={20}>"Il semblerait que vous n'avez pas fait de recherche"</Box>
-        </Typography>
-      )
-    }
-  }
-
-  const renderResult = () => (
-    !isLoading && <Grid container spacing={1}>
-      {data && data.map((item, index) => (
-        <Grid item xs={3} spacing={3} key={index}>
-          <Tile
-            title={item.name}
-            picture={item.picture}
-            url={`${WASABI_ARTIST_SEARCH}/${item.name}`}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  )
-
   return (
-    <div>
+    <BrowserRouter>
       <TopBar />
       <Container maxWidth="lg">
-        <Box pt={2}>
-          <Typography variant="h5" component="h2">
-            <Box textAlign="center">Bienvenue, veuillez taper le nom de votre groupe préferé en bas:</Box>
-          </Typography>
-          <Box mt={2} width={1 / 2} mx="auto" mb={2}>
-            <form noValidate autoComplete="off">
-              <Search
-                placeholder="Rechercher un nom de groupe ici"
-                searchFunction={searchHandler} />
-            </form>
-          </Box>
-          <Box pt={2} pb={2}>
-            {renderResult()}
-            {renderLoader()}
-            {renderNoResult()}
-          </Box>
-        </Box>
+        <Route exact path="/" component={Home} />
+        <Route path="/artist/:id" component={Detail} />
       </Container>
-    </div>
+    </BrowserRouter>
   );
 }
 
