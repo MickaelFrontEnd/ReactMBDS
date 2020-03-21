17021793b9b42f1cb1c477dfba0e3b6321fa4821
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2020-03-21 21:37:39 +0400

    Add url config

---
+++ MusicBand/music-band/src/cofig/Url.js
@@ -0,0 +1,9 @@
+// This file contains all necessary Webservice URL
+
+export const WASABI_ROOT = 'https://wasabi.i3s.unice.fr';
+
+export const WASABI_SEARCH = `${WASABI_ROOT}/search`;
+
+export const WASABI_FULLTEXT_SEARCH = `${WASABI_SEARCH}/fulltext`;
+
+export default WASABI_FULLTEXT_SEARCH;
