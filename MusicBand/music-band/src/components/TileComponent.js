5b2a1b33b2fd0860d645b843a76e169f75a8c9a2
Iony Ranarilala <ionyranarilala@gmail.com>
2020-03-21 21:41:03 +0400

    Add tile component

---
+++ MusicBand/music-band/src/components/TileComponent.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  media: {
+    height: 200,
+  },
+  content: {
+    height: 50
+  },
+  title: {
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    overflow: "hidden"
+  }
+});
+
+const Tile = ({ title, picture }) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={picture}
+          title={title}
+        />
+        <CardContent className={classes.content}>
+          <Typography gutterBottom variant="h5" component="h2" className={classes.title}>{title}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Voir plus
+        </Button>
+      </CardActions>
+    </Card>
+  )
+};
+
+Tile.propTypes = {
+  title: PropTypes.string,
+  picture: PropTypes.string
+};
+
+export default Tile;
