14219ddbf9e6214724cf30243c0d904429fb5aab
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2020-03-22 01:49:59 +0400

    Move project outside to fix heroku deployment

---
+++ build/precache-manifest.51705c427f4138da4a17eb254eb61281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760b4afb5e14e24a2d855923d4b8015e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6620bf7cfadc31e9ff5",
+    "url": "/static/js/2.3978d016.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/static/js/2.3978d016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f3102c48eeff0136ac",
+    "url": "/static/js/main.a87acd0e.chunk.js"
+  },
+  {
+    "revision": "122433c3a1492b845e71",
+    "url": "/static/js/runtime-main.fef7ba2d.js"
+  }
+]);
