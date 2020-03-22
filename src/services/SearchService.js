903dec6d69f80678b107932e5e1339a9ac999911
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2020-03-22 16:37:27 +0400

    Auto stash before merge of "master" and "origin/master"

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -1,13 +1,13 @@
 import WASABI_FULLTEXT_SEARCH, { WASABI_ARTIST_SEARCH } from '../cofig/Url';
 
 export const searchByFullText = (terms) => {
-  return fetch(`${WASABI_FULLTEXT_SEARCH}/${terms}`)
+    return fetch(`${WASABI_FULLTEXT_SEARCH}/${terms}`)
     .then((result) => result.json())
     .catch((err) => console.log(err));
 }
 
 export const searchByArtist = (artist) => {
-  return fetch(`${WASABI_ARTIST_SEARCH}/${artist}`)
+    return fetch(`${WASABI_ARTIST_SEARCH}/${artist}`)
     .then((result) => result.json())
     .catch((err) => console.log(err));
 }
