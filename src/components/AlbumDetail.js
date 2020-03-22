351b9f9a2e35e92a03c9b97d6959de578eab8c85
Iony Ranarilala <ionyranarilala@gmail.com>
2020-03-22 19:33:41 +0400

    Add album detail

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const AlbumDetail = ({ open, onClose, album }) => {
+  return (
+    <Dialog aria-labelledby="album-dialog" open={open} onClose={onClose} maxWidth="sm" fullWidth>
+      <Box p={2}>
+        <Box textAlign="center" mb={2}>
+          <Box mb={2}>
+            <img src={album.cover.medium} alt={album.title} />
+          </Box>
+          <Typography component="h3" variant="h5">{album.title}</Typography>
+        </Box>
+        <List component="nav" aria-label="song list">
+          {album.songs?.map((song) => (
+            <ListItem key={song._id}>
+              <ListItemText primary={`${song.position + 1}) ${song.title}`} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Dialog>
+  )
+};
+
+export default AlbumDetail;
