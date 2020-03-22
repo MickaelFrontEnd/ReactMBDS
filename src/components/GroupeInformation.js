fdd469997f46131365f49f93ab08d13cbe9700de
Iony Ranarilala <ionyranarilala@gmail.com>
2020-03-22 17:06:37 +0400

    Add groupe information

---
+++ src/components/GroupeInformation.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
+
+const GroupInformation = ({ data }) => (
+  <TableContainer component={Paper}>
+    <Table>
+      <TableBody>
+        <TableRow key={1}>
+          <TableCell>Date de naissance</TableCell>
+          <TableCell>{data.lifeSpan.begin} {data.lifeSpan.end !== '' && <span> -> {data.lifeSpan.end}</span>}</TableCell>
+        </TableRow>
+        <TableRow key={2}>
+          <TableCell>Location</TableCell>
+          <TableCell>{data.location.country} - {data.location.city}</TableCell>
+        </TableRow>
+        <TableRow key={3}>
+          <TableCell>Genre</TableCell>
+          <TableCell>{data.genres.join(' - ')}</TableCell>
+        </TableRow>
+        <TableRow key={4}>
+          <TableCell>Label</TableCell>
+          <TableCell>{data.labels.join(' - ')}</TableCell>
+        </TableRow>
+        <TableRow key={5}>
+          <TableCell>Nombre de fans</TableCell>
+          <TableCell>{data.deezerFans}</TableCell>
+        </TableRow>
+        <TableRow key={6}>
+          <TableCell>Artiste(s) associé(s)</TableCell>
+          <TableCell>{data.associatedMusicalArtist.join(' - ')}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
+
+export default GroupInformation;
