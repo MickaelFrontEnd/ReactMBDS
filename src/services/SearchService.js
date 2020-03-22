c97873459e2961f5de0e82dab78a4576c3674e5a
Iony Ranarilala <ionyranarilala@gmail.com>
2020-03-22 16:33:48 +0400

    Search by artist/group

--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -1,9 +1,15 @@
-import WASABI_FULLTEXT_SEARCH from '../cofig/Url';
+import WASABI_FULLTEXT_SEARCH, { WASABI_ARTIST_SEARCH } from '../cofig/Url';
 
-const searchByFullText = (terms) => {
+export const searchByFullText = (terms) => {
     return fetch(`${WASABI_FULLTEXT_SEARCH}/${terms}`)
-    .then((result) => result.json())
-    .catch((err) => console.log(err));
+        .then((result) => result.json())
+        .catch((err) => console.log(err));
+}
+
+export const searchByArtist = (artist) => {
+    return fetch(`${WASABI_ARTIST_SEARCH}/${artist}`)
+        .then((result) => result.json())
+        .catch((err) => console.log(err));
 }
 
 export default searchByFullText;
